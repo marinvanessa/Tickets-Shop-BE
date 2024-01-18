61609400dd1cb27b1a3945786f5fee8b2a4ea2db
Vanessa <marinvanessaramona38@gmail.com>
2024-01-18 18:55:10 +0200

    Added notifications when adding a ticket to shopping cart.

--- src/main/java/com/example/tickets/service/impl/JWTServiceImpl.java
+++ src/main/java/com/example/tickets/service/impl/JWTServiceImpl.java
@@ -22,7 +22,7 @@ public class JWTServiceImpl implements JWTService {
     public String generateToken(UserDetails userDetails) {
         return Jwts.builder().setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 40))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
